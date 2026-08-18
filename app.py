85c5b4e4ad18b46b4ab5871c1e7d7c036aa2ebcc
DA-Py-agent <ashwinkumar53172@gmail.com>
2026-08-18 15:36:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ with st.sidebar:
 
 if api_key:
     genai.configure(api_key=api_key)
-    model = genai.GenerativeModel('gemini-pro')
+    model = genai.GenerativeModel('gemini-1.5-flash')
 
     # File Uploader
     uploaded_file = st.file_uploader("Upload your Excel or CSV file", type=['csv', 'xlsx'])
